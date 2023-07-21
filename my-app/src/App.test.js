f2ffcece37a5527df0817fda205f03f43df71800
taylor-russon <twrusson@uw.edu>
2023-07-20 17:59:33 -0700

    tests

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,35 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event"
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // Checking if checkbox is not checked v
+  expect(screen.getByTestId("checkbox")).not.toBeChecked()
+
+  // Event: Click on the checkbox v
+  userEvent.click(screen.getByTestId("checkbox"));
+
 });
+
+
+test("testing button", () => {
+  render(<App />);
+  //my test 
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+});
+
+
+test("testing text", () => {
+  render(<App />);
+  //extra test
+  expect(screen.getByText("Hello World")).toBeInTheDocument();
+});
+
+
+
+
+//npm run test
